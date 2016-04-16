5a11404d18840bd6b507aed2bfc55adccbf54a4c
John S. Ryan <jtigger@infosysengr.com>
2016-04-16 07:27:36 -0700

    Log Activity lifecycle

--- app/src/main/java/com/infosysengr/geoquiz/QuizActivity.java
+++ app/src/main/java/com/infosysengr/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.infosysengr.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +13,8 @@ public class QuizActivity extends AppCompatActivity implements QuizListener {
     private Button falseButton;
     private Button maybeButton;
     private Quiz quiz;
+
+    private static final String TAG = "QuizActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +70,36 @@ public class QuizActivity extends AppCompatActivity implements QuizListener {
                 Toast.LENGTH_LONG)
                 .show();
         displayNextQuestion();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
 
